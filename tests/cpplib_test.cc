807aaf43bd3b2e5cb28d3db5a7a156b4712b0cc5
hongshuochen <hongshuo@usc.edu>
2020-08-27 15:56:03 -0700

    update cpp_test

--- tests/cpplib_test.cc
+++ tests/cpplib_test.cc
@@ -4,11 +4,3 @@
 #include <vector>
 
 #include "gtest/gtest.h"
-
-TEST(CPPLibTest, ReturnHelloWorld) {
-  CPPLib cpplib;
-  std::string actual = cpplib.PrintHelloWorld();
-  std::string expected = "**** Hello World ****";
-  EXPECT_EQ(expected, actual);
-}
-
